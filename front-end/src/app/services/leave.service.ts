159cccd05a0ec45661b0c2c3a10c2d3344f1c813
Kexin Wu <408294835@qq.com>
2018-12-18 09:48:32 +0800

    完成 leave, out, record service

--- front-end/src/app/services/leave.service.ts
+++ front-end/src/app/services/leave.service.ts
@@ -1,9 +1,134 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Leave } from "../domain/leave";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaveService {
 
-  constructor() { }
+  private api_url = "http://localhost:3000/leaves";
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  /**
+   * @description 获取所有的 leave
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @returns {Observable<Leava[]>}
+   * @memberof LeaveService
+   */
+  getLeaves(): Observable<Leave[]> {
+    let url = `${this.api_url}`;
+    return this.http.get<Leave[]>(url);
+  };
+
+  /**
+   * @description 根据 id 获取 leave
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @param {string} id
+   * @returns {Observable<Leave>}
+   * @memberof LeaveService
+   */
+  getLeaveById(id: string): Observable<Leave> {
+    let url = `${this.api_url}/${id}`;
+    return this.http.get<Leave>(url);
+  };
+
+  /**
+   * @description get a leave by userId
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @param {string} userId
+   * @returns {Observable<Leave>}
+   * @memberof LeaveService
+   */
+  getLeaveByUserId(userId: string): Observable<Leave> {
+    let url = `${this.api_url}/?userId=${userId}`;
+    return this.http.get<Leave>(url);
+  };
+
+  /**
+   * @description new a leave 
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @param {Leave} leave
+   * @returns {Observable<Leave>}
+   * @memberof LeaveService
+   */
+  createLeave(leave: Leave): Observable<Leave> {
+    let url = `${this.api_url}`;
+    return this.http.post<Leave>(url, leave, this.httpOptions);
+  };
+
+  /**
+   * @description update a leave
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @param {Leave} leave
+   * @returns {Observable<Leava>}
+   * @memberof LeaveService
+   */
+  updateLeave(leave: Leave): Observable<Leave> {
+    let url = `${this.api_url}/${leave.id}`;
+    return this.http.put<Leave>(url, leave, this.httpOptions);
+  };
+
+  /**
+   * @description delete a leave by the leave object
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @param {Leave} leave
+   * @returns {Observable<{}>}
+   * @memberof LeaveService
+   */
+  deleteLeave(leave: Leave): Observable<{}> {
+    let url = `${this.api_url}/${leave.id}`;
+    return this.http.delete(url, this.httpOptions);
+  };
+
+  /**
+   * @description delete a leave by this id
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @param {string} id
+   * @returns {Observable<{}>}
+   * @memberof LeaveService
+   */
+  deleteLeaveById(id: string): Observable<{}> {
+    let url = `${this.api_url}/${id}`;
+    return this.http.delete(url, this.httpOptions);
+  };
+
+  /**
+   * @description delete a leave by userId
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @param {string} userId
+   * @returns {Observable<{}>}
+   * @memberof OutService
+   */
+  deleteLeaveByUserId(userId: string): Observable<{}> {
+    let url = `${this.api_url}/?userId=${userId}`;
+    return this.http.delete(url, this.httpOptions);
+  };
+
+  /**
+   * @description get the new leave item's id
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @returns {string}
+   * @memberof LeaveService
+   */
+  createNewLeaveId(): string {
+    let currentTime = new Date();
+    return currentTime.getTime().toString();
+  };
 }
--- front-end/src/app/services/out.service.ts
+++ front-end/src/app/services/out.service.ts
@@ -1,9 +1,150 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Out } from "../domain/out";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OutService {
 
-  constructor() { }
+  private api_url = "http://localhost:3000/outs";
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) { }
+
+  // 使用方法
+  // 1. this.userService.getConfig().subscribe(
+  //        (data: Config) => this.config = { ...data }
+  //    );
+  // 2. this.userService.getConfigResponse()
+  //   .subscribe(resp => {
+  //     const keys = resp.headers.keys();
+  //     this.headers = keys.map(key =>
+  //       `${key}: ${resp.headers.get(key)}`);
+  //     this.config = { ... resp.body };
+  //   });
+
+
+  /**
+   * @description 查询所有外出申请
+   * @author Wu Kexin
+   * @date 2018-12-15
+   * @returns {Observable<Out[]>}
+   * @memberof OutService
+   */
+  getOuts(): Observable<Out[]> {
+    let url = `${this.api_url}`;
+    return this.http.get<Out[]>(url);
+    // return this.http.get<Out[]>(url, { observe: 'response' });
+  };
+
+  /**
+   * @description 根据 Id 查询 out
+   * @author Wu Kexin
+   * @date 2018-12-15
+   * @param {string} id
+   * @returns {Observable<Out>}
+   * @memberof OutService
+   */
+  getOutById(id: string): Observable<Out> {
+    let url = `${this.api_url}/${id}`;
+    return this.http.get<Out>(url);
+  };
+
+
+  /**
+   * @description 根据 userId 查询 out
+   * @author Wu Kexin
+   * @date 2018-12-15
+   * @param {string} userId
+   * @returns {Observable<Out>}
+   * @memberof OutService
+   */
+  getOutByUserId(userId: string): Observable<Out> {
+    let url = `${this.api_url}/?userId=${userId}`;
+    return this.http.get<Out>(url);
+  };
+
+  /**
+   * @description 新建一个 out
+   * @author Wu Kexin
+   * @date 2018-12-15
+   * @param {Out} out
+   * @returns {Observable<Out>}
+   * @memberof OutService
+   */
+  createOut(out: Out): Observable<Out> {
+    let url = `${this.api_url}`;
+    return this.http.post<Out>(url, out, this.httpOptions);
+    // .pipe(
+    //   catchError(this.handleError('addHero', hero))
+    // )
+  };
+
+  /**
+   * @description 修改某个 out 的信息
+   * @author Wu Kexin
+   * @date 2018-12-15
+   * @param {Out} ouot
+   * @returns {Observable<Out>}
+   * @memberof OutService
+   */
+  updateOut(out: Out): Observable<Out> {
+    let url = `${this.api_url}/${out.id}`;
+    return this.http.put<Out>(url, out, this.httpOptions)
+  };
+
+  /**
+   * @description delete a out by object out
+   * @author Wu Kexin
+   * @date 2018-12-15
+   * @param {Out} out
+   * @returns {Observable<{}>}
+   * @memberof OutService
+   */
+  deleteOut(out: Out): Observable<{}> {
+    let url = `${this.api_url}/${out.id}`;
+    return this.http.delete(url, this.httpOptions);
+  };
+
+  /**
+   * @description delete a out by id
+   * @author Wu Kexin
+   * @date 2018-12-15
+   * @param {string} id
+   * @returns {Observable<{}>}
+   * @memberof OutService
+   */
+  deleteOutById(id: string): Observable<{}> {
+    let url = `${this.api_url}/${id}`;
+    return this.http.delete(url, this.httpOptions);
+  };
+
+  /**
+   * @description 根据 userId 删除 Out
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @param {string} userId
+   * @returns {Observable<{}>}
+   * @memberof OutService
+   */
+  deleteOutByUserId(userId: string): Observable<{}> {
+    let url = `${this.api_url}/?userId=${userId}`;
+    return this.http.delete(url, this.httpOptions);
+  }
+
+  /**
+   * @description 获取新建 out 的 ID
+   * @author Wu Kexin
+   * @date 2018-12-18
+   * @returns {string}
+   * @memberof OutService
+   */
+  createNewOutId(): string {
+    let currentTime = new Date();
+    return currentTime.getTime().toString();
+  }
 }
