66eb505048ea02e4e8c09990c2507cea0d8101d8
sqq7777 <45021084+sqq7777@users.noreply.github.com>
2018-12-28 15:38:49 +0800

    Update login.component.ts

--- front-end/src/app/pages/login/login.component.ts
+++ front-end/src/app/pages/login/login.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {NzMessageService} from "ng-zorro-antd";
 
-// * @author Fan Lishui
+// * @author Sun Qisong
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
