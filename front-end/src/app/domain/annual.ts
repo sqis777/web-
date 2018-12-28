31330fad81763b808a5134a821653c8cdc5b7d08
一只咕叽 <mandy@wukexindeMacBook.local>
2018-12-28 15:12:25 +0800

    缺少的文件

---
+++ front-end/src/app/domain/annual.ts
@@ -0,0 +1,14 @@
+/**
+ * @description 所有员工年假信息实体类
+ * @author Sun Qisong
+ * @date 2018-12-15
+ * @export
+ * @class Data
+ */
+export class dataSet {
+    id: string;
+    name: string;
+    age: number;
+    remain: number;
+    total: number;
+}
